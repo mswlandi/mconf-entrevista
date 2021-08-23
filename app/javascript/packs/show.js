05fdd8086905f021840819cda68cc0a7680e2c7f
mswlandi <fakepsnmarcos@gmail.com>
2021-08-23 02:39:28 -0300

    Stylize and Capitalise Mconf occurences in messages

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,8 +1,11 @@
+colorPalette = ["#481E9D", "#8D74C0", "#CA4B57", "#2B2D42", "#1C77C3", "#81B29A", "#E98039"]
+
 form = document.getElementsByTagName("form")[0];
 inputText = document.getElementById("message_content");
 
 backgroundIsColored = false;
 
+// Client-side form validation
 form.onsubmit = (event) => {
     errorTags = document.getElementsByClassName("error");
 
@@ -14,8 +17,8 @@ form.onsubmit = (event) => {
     }
 };
 
+// Checks wether there's a Mconf (case insensitive) and toggles the background color accordingly
 inputText.oninput = () => {
-    colorPalette = ["#481E9D", "#8D74C0", "#CA4B57", "#2B2D42", "#1C77C3", "#81B29A", "#E98039"]
     body = document.getElementsByTagName("body")[0];
 
     if (inputText.value.toLowerCase().includes("mconf")) {
@@ -30,3 +33,16 @@ inputText.oninput = () => {
         body.style.background = 'white';
     }
 };
+
+// Stylize occurences of "Mconf" in the messages
+window.onload = () => {
+    messages = document.getElementsByClassName("content");
+    mconfString = '<span style="color: randomColor">Mconf</span>'
+
+    seedCounter = 0;
+
+    Array.from(messages).forEach((tag) => {
+        randomColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
+        tag.innerHTML = tag.innerHTML.replace(/Mconf/g, mconfString.replace("randomColor", randomColor));
+    });
+}
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ApplicationRecord
-    validates :content, presence: true
+  validates :content, presence: true
+
+  def content=(val)
+    write_attribute(:content, val.gsub(/Mconf/i, "Mconf"))
+  end
 end
