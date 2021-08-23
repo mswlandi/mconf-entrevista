43f3431bd3b011f729151096ec64b7fbbbc64c30
mswlandi <fakepsnmarcos@gmail.com>
2021-08-23 00:24:58 -0300

    bg color changes if mconf shows up in input

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,13 +1,32 @@
 form = document.getElementsByTagName("form")[0];
+inputText = document.getElementById("message_content");
 
-error_tags = document.getElementsByClassName("error");
+backgroundIsColored = false;
 
 form.onsubmit = (event) => {
-    inputText = document.getElementById("message_content");
+    errorTags = document.getElementsByClassName("error");
+
     if (inputText.value.trim() == '') {
         event.preventDefault();
-        Array.from(error_tags).forEach((error_tag) => {
-            error_tag.style.display = "block";
+        Array.from(errorTags).forEach((errorTag) => {
+            errorTag.style.display = "block";
         });
+    }
+};
+
+inputText.oninput = () => {
+    colorPalette = ["#481E9D", "#8D74C0", "#CA4B57", "#2B2D42", "#1C77C3", "#81B29A", "#E98039"]
+    body = document.getElementsByTagName("body")[0];
+
+    if (inputText.value.toLowerCase().includes("mconf")) {
+        if (!backgroundIsColored) {
+            backgroundIsColored = true;
+            randomColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
+            body.style.background = randomColor;
+        }
+    }
+    else if (backgroundIsColored){
+        backgroundIsColored = false;
+        body.style.background = 'white';
     }
 };
